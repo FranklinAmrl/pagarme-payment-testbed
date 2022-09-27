3b12c64c6a2fd913c8db41db7f1f269e5d963c3a
Franklin <franklinamaralt@gmail.com>
2022-09-27 17:27:22 -0300

    Created minimal project  settings

---
+++ payment/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+urlpatterns = [
+    path("", name="home"),
+    path("", name="pix_payment"),
+    path("", name="card_payment"),
+    path("", name="card_registration"),
+]
